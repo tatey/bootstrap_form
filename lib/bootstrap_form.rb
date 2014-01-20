86102cdcb14e79bb27c91f43f62d4aa0cc6308e4
Tate Johnson <tate@tatey.com>
2014-01-21 07:49:04 +1100

    Define BootstrapForm before loading classes.

--- lib/bootstrap_form.rb
+++ lib/bootstrap_form.rb
@@ -1,7 +1,8 @@
 require 'action_view'
-require 'bootstrap_form/builder'
-require 'bootstrap_form/group'
-require 'bootstrap_form/version'
 
 module BootstrapForm
 end
+
+require 'bootstrap_form/builder'
+require 'bootstrap_form/group'
+require 'bootstrap_form/version'
